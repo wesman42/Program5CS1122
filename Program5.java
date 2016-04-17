1fed6f786e43dad768ade867196827bd768f1638
kcarchin <kcarchin@mtu.edu>
2016-04-17 11:39:54 -0400

    Program5.java
    The actual program.

---
+++ Program5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+public class Program5 {
+
+public void run(int port, String rootFolder) throws Exception {
+	ServerSocket SVRSOCK = new ServerSocket(port);
+	Socket SOCK = SVRSOCK.accept();
+	InputStreamReader IR = new InputStreamReader(SOCK.getInputStream());
+	BufferedReader BR = new BufferedReader(IR);
+	
+	
+	String message = BR.readLine();
+	System.out.println(message);
+}
+	
+	public static void main(String[] args) throws Exception {
+
+		Program5 svr = new Program5();
+		svr.run(2112, "Documents");
+
+	}
+
+}
