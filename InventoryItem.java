e1689b1d834d5ae0a02244c263c7e1cabf368be9
wesman42 <wmharris@mtu.edu>
2016-04-23 16:36:07 -0400

    Added files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,13 @@
+
+public class InventoryItem {
+String name;
+int stockQuantity;
+double costPerItem;
+
+public InventoryItem(String name, int num, double cost){
+	this.name = name;
+	stockQuantity = num;
+	costPerItem = cost;
+}
+
+}
---
+++ Orders.java
@@ -0,0 +1,18 @@
+
+public class Orders {
+String name;
+InventoryItem item;
+int quantity;
+
+public Orders(String name, InventoryItem item, int quantity ) {
+	this.name = name;
+	this.item = item;
+	this.quantity = quantity;
+}
+
+public boolean getStatus() {
+	
+	return false;
+}
+
+}
